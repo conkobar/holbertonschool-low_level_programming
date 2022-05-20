ab312200c94a14c3534b0084ff057ea6903708fd
conkobar <connorkleine@gmail.com>
2022-05-20 13:39:00 -0500

    idk what i am doing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+
+/* this is a function to print a programming quip */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	getchar();
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

f0c4a46de5c0a0b112efcea2929567e632dc2863
conkobar <connorkleine@gmail.com>
2022-05-20 14:49:09 -0500

    adv task 8, first try

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - whatever
+ *
+ * Return: whatever
+ */
+#include<stdio.h>
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (0);
+}

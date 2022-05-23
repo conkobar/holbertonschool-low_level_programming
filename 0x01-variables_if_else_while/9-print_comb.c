bbb2123ec411ff23f0716105811622052f9e05ea
conkobar <connorkleine@gmail.com>
2022-05-23 14:13:43 -0500

    blunt force trauma update to task 8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 48 ; i < 58 ; i++)
+	for (i = 48 ; i < 57 ; i++)
 	{
 		putchar(i);
 		putchar(44);
 		putchar(32);
 	}
+	putchar(58);
 	putchar(10);
 	return (0);
 }

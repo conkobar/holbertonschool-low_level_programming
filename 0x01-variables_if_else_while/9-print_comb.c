a19e017a8add2e3d6b8f41e57d87b3d50246736c
conkobar <connorkleine@gmail.com>
2022-05-23 14:11:37 -0500

    task 8, first try

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+/**
+ * main - idk
+ *
+ * Return: idk
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i < 58 ; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}

021c2b49043c2b983471e6e507f7390f33ab2637
conkobar <connorkleine@gmail.com>
2022-05-23 12:51:46 -0500

    changed 4 to while statement

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,14 @@
 
 int main(void)
 {
-	char i;
+	char i = 'a';
 
-	for (i = 'a' && i != 'e' && i != 'q' ; i <= 'z' ; i++)
-		putchar(i);
+	while (i <= 'z')
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+		i++;
+	}
 	putchar('\n');
 	return (0);
 }

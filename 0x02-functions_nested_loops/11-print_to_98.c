ded7b8d5918b73208942b0d306dd7a485f68f775
conkobar <connorkleine@gmail.com>
2022-05-24 15:57:49 -0500

    task 11 really hitting the end of my thought train here

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,14 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
-		printf("%i, ", n++);
-	else if (n > 98)
-		printf("%i, ", n--);
-	else
-		printf("%i", n);
+	if (n != 98)
+	{
+		while (n < 98)
+			printf("%i, ", n++);
 
-	putchar('\n');
+		while (n > 98)
+			printf("%i, ", n--);
+	}
+	else
+		printf("%i\n", n);
 }

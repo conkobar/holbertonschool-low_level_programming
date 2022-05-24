b1e36d741b7ef76bd02d7b69a9553bbce524351d
conkobar <connorkleine@gmail.com>
2022-05-24 15:18:45 -0500

    task 11, first try

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - idk
+ * print_to_98 - idk
  *
  * Return: idk
  */
 
-int main(void)
+void print_to_98(void)
 {
+	int i;
 
+	while (i < 98)
+	{
+		printf("%i, ", i++);
+
+		if (i == 98)
+			printf("98");
+	}
+}

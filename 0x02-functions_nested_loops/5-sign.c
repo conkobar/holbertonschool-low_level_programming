b417dbf3b5ea11966195cdcbce5bcaef04c0f2bd
conkobar <connorkleine@gmail.com>
2022-05-24 12:56:19 -0500

    task 5, try 1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,26 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - idk
- *
+ * print_sign - idk
+ * @n: idk
  * Return: idk
  */
 
-int main(void)
+int print_sign(int n)
 {
-
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

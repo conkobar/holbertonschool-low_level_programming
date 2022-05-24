3fcddcea4425fd479bbe8076f298edc9344a442d
conkobar <connorkleine@gmail.com>
2022-05-24 13:26:05 -0500

    found out return values are enough on 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _abs - absoluter value
- * @i: integer
+ * _abs - idk
+ * @i: idk
  * Return: idk
  */
 
 int _abs(int i)
 {
 	if (i < 0)
-		_putchar(i + i + i);
+		return (-i);
 	else
-		_putchar(i);
+		return (i);
 }

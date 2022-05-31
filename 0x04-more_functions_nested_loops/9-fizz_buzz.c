38f92d092812092206bc73308a30541a6d879725
conkobar <connorkleine@gmail.com>
2022-05-31 18:58:49 -0500

    i think the space after the last word was making me fail checker, task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,26 +3,23 @@
 /**
  * main - code
  *
- * Return: 0
+ * Return: o
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
-			printf("%d", i);
-
-			if (i == 100)
-				putchar(32);
+			printf("%d ", i);
 	}
-	putchar(10);
+	printf("Buzz\n");
 	return (0);
 }

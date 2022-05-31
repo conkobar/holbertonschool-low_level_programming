fa1dd86155abcb8961e06775ad9f2b88a3de6ac3
conkobar <connorkleine@gmail.com>
2022-05-31 12:29:20 -0500

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	while (i++)
+	while (i < 10)
 	{
-		if ((i >= 0 && i < 10) && (i != 2 || i != 4))
-		_putchar(i);
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		i++;
 	}
 	_putchar(10);
 }

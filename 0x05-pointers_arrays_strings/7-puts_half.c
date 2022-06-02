27400f4ce95dd0092c47373c659a0309bf10aa08
conkobar <connorkleine@gmail.com>
2022-06-02 16:13:35 -0500

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - prints half of a string
+ * @str: char
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i, len, mid;
+
+	while (str++)
+		len++;
+
+	if (len % 2 == 0)
+		mid = len / 2;
+
+	else
+		mid = (len + 1) / 2;
+
+	for (i = mid; i < len; index++)
+		_putchar(str[i]);
+
+	_putchar(10);
+}

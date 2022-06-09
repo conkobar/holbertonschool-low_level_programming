4c5eb6f330cb0827537a7dda69924fd3273e0ced
conkobar <connorkleine@gmail.com>
2022-06-08 21:43:40 -0500

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * prime_r - prime # recursion
+ * @i: int
+ * @j: int
+ * Return: 1 if int is prime, else 0
+ */
+
+int prime_r(int i, int j)
+{
+	if (j == 1)
+		return (1);
+
+	if (i % 1 == 0 && j > 0)
+		return (0);
+
+	return (prime_r(i, i - 1));
+}
+
+/**
+ * is_prime_number - identifies prime numbers
+ * @n: int
+ * Return: 1 if int is prime, else 0
+ */
+
+int is_prime_number(int n);
+{
+	if (n <= 1)
+		return (0);
+
+	return (prime_r(n, n - 1));
+}

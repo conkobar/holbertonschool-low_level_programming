67bbb8efa87d7737b81f5033acc17ba69a367dcb
conkobar <connorkleine@gmail.com>
2022-06-16 15:16:43 -0500

    t1: unsure why blank input not returning null

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,11 +12,14 @@ char *_strdup(char *str)
 	char *dupe;
 	int i, len;
 
+	if (str == NULL)
+		return (NULL);
+
 	for (len = 0; str[len]; len++)
 
 	dupe = malloc(sizeof(char) * (len + 1));
 
-	if (str == NULL || dupe == NULL)
+	if (dupe == NULL)
 		return (NULL);
 
 	for (i = 0; i < len; i++)

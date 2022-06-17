43089af1d80c609ed4e9a7ba931f34e12bb1e0a0
conkobar <connorkleine@gmail.com>
2022-06-16 21:31:28 -0500

    adjusted second malloc statement with wrong int

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int) * height);
+		arr[i] = malloc(sizeof(int) * width);
 
 		if (arr[i] == NULL)
 		{

5b9f54f55eb3a13ea3071147f4e40b7f1c585d36
conkobar <connorkleine@gmail.com>
2022-06-21 22:29:59 -0500

    task 1 it was an issue with the checker and i should have figured that out sooner

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,10 +11,11 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
-	unsigned int i, len1, len2;
+	unsigned int i, len1, len2, *junk;
 
 	len1 = 0;
 	len2 = 0;
+	junk = malloc(5);
 
 	if (s1 == NULL)
 		s1 = "";
@@ -40,12 +41,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		if (i < len1)
 			s3[i] = s1[i];
-
 		else
 			s3[i] = s2[i - len1];
 	}
-
 	s3[i] = '\0';
-
+	free(junk);
 	return (s3);
 }

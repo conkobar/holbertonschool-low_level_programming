d522bea40666d9d23020fd5198ff3b697adda770
conkobar <connorkleine@gmail.com>
2022-06-23 16:00:32 -0500

    edit to get_op_func on task 3, missing checks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,9 +22,9 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (i < 5 && s)
+	while (ops[i].op)
 	{
-		if (*s == *ops[i].op)
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}

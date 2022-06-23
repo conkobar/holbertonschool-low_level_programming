a994fcfcd7d38ec6bbe4d1a73d5a1504159066c8
conkobar <connorkleine@gmail.com>
2022-06-23 15:54:32 -0500

    task 3 is a doozy

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,46 @@
+#include "3-calc.h"
+
+/**
+ * main - main
+ * @argc: argc
+ * @argv: argv
+ * Return: void
+ */
+
+int main(int argc, char **argv)
+{
+	char x;
+	int arg1, arg2, result;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	x = *argv[2];
+
+	if ((x == '/' || x == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - does a + b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - does a - b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - does a * b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - does a / b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - does a % b
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a % b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

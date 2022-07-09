c3dd8855cb84467b2001a54036870f66c636c4fc
conkobar <connorkleine@gmail.com>
2022-07-08 22:28:32 -0500

    had extra character in printf func

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,18 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count;
 
-	while (h)
+	for (count = 0; h; count++)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 
 		else
-			printf("[%u] ^%s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
-		count++;
 	}
 	return (count);
 }

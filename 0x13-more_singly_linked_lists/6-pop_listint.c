d190841dcd34c5a25aaa95ee9aaa7fe80c3a8bec
conkobar <connorkleine@gmail.com>
2022-07-12 18:14:26 -0500

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head of list & returns the node's data
+ * @head: list
+ * Return: deleted node's data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (!head)
+		return (0);
+
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (n);
+}

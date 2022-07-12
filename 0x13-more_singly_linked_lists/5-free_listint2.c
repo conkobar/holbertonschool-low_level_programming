3a95fca38f2725c76484d17b625d889486903fdf
conkobar <connorkleine@gmail.com>
2022-07-12 10:44:54 -0500

    task 5, maybe this time

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,16 @@
 
 void free_listint2(listint_t **head)
 {
+	listint_t *temp = *head;
+
+	if (!*head)
+		return;
 
 	while (*head)
 	{
+		temp = (*head)->next;
 		free(*head);
-		*head = NULL;
+		*head = temp;
 	}
+	*head = NULL;
 }

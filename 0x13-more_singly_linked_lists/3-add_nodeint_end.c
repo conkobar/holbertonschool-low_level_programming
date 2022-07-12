01e00e6878b8d619c002a2e940d72482c3da4662
conkobar <connorkleine@gmail.com>
2022-07-12 09:53:01 -0500

    task 5

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new = malloc(sizeof(listint_t));
 
 	if (!new)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->n = n;
 	new->next = NULL;
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,7 +7,10 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
 
 	while (*head)
-
+	{
+		free(*head);
+		*head = NULL;
+	}
+}

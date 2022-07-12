1ed529d2a170ce911b51e69df28ffd422fe9b956
conkobar <connorkleine@gmail.com>
2022-07-12 10:50:39 -0500

    task 5 edge cases driving me crazy

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
+	if (*head == NULL)
+		return;
+
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+	*head = NULL;
 }

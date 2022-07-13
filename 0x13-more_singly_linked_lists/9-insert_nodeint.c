26833db858d38cd588a53a94fe3a14325d331bb9
conkobar <connorkleine@gmail.com>
2022-07-12 19:50:38 -0500

    task 9, adjusted assignments within for loop

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,10 +28,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	for (i = 0; i < idx && temp; i++)
-		temp = temp->next;
-
-	new->next = temp->next;
-	temp->next = new;
-
-	return (new);
+	{
+		if (i == (idx - 1))
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+	return (new);	
 }

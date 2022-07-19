eb5f7bc98aa8d9379f6066b0795e00c52a0095fb
conkobar <connorkleine@gmail.com>
2022-07-18 20:43:04 -0500

    added project 0x15; task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: name of given file
+ * @letters: letters
+ * Return: 0 if given null
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+        char *bufr = malloc(sizeof(char) * letters);
+        int x, y, z;
+
+        if (!bufr || !filename)
+                return (0);
+
+        x = open(filename, O_RDONLY);
+        if (x < 0)
+                return (0);
+
+        y = read(x, bufr, letters);
+        if (y < 0)
+        {
+                free(bufr);
+                return (0);
+        }
+
+        bufr[y] = '\0';
+
+        close(x);
+
+        z = write(STDOUT_FILENO, bufr, y);
+        if (z < 0)
+        {
+                free(bufr);
+                return (0);
+        }
+
+        free(bufr);
+        return (z);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,6 @@
+# [<center>0x16. C - File I/O - 1141</center>](https://intranet.hbtn.io/projects/1141)
+ ---
+ ### <center>Description</center> 
+ ##### &emsp; Working with file descriptors for Holberton School
+ ---
+ [<center>Buzz</center>](github.com/conkobar)

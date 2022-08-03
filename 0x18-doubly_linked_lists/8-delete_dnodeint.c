8915b799178fc85566245b4cf2d0a6c7eac9361c
conkobar <connorkleine@gmail.com>
2022-08-02 20:46:30 -0500

    task 8

---
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint - deletes the node @ index of a list
+ * @head: list
+ * @index: member # of element to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *tmp = *head;
+
+	if (*head)
+	{
+		for (i = 0; i < index; i++)
+			tmp = tmp->next;
+
+
+		if (tmp)
+		{
+			if (*head == tmp)
+				*head = tmp->next;
+
+			if (tmp->prev)
+				tmp->prev->next = tmp->next;
+
+			if (tmp->next)
+				tmp->next->prev = tmp->prev;
+
+			return (1);
+		}
+	}
+
+	return (-1);
+}

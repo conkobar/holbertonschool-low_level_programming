fb1ca753d518c623176f51f72efc72a7897208ba
conkobar <connorkleine@gmail.com>
2022-08-04 13:19:39 -0500

    missing checks on 1, saays requirement. not sure what it is

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: ptr to the table, or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i;
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+
+	if (!ht)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_table_t) * size);
+
+	if (!ht->array)
+		return (NULL);
+
+	ht->size = size;
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives index of a key
+ * @key: key
+ * @size: size of the hash table array
+ *
+ * Return: index of key/value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int i;
+}

960c359988dc5746a67d0920cfd91ee2f4940c21
conkobar <connorkleine@gmail.com>
2022-08-04 18:36:11 -0500

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int idx, punk;
+
+	printf("{");
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		node = ht->array[idx];
+
+		for (node = ht->array[idx]; node; node = node->next)
+		{
+			if (punk)
+				printf(", ");
+
+			printf("'%s': '%s'", node->key, node->value);
+			punk = 69;
+		}
+	}
+	printf("}\n");
+}

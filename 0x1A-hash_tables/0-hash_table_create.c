db59b1131c01f3a06ecaa81ed850a130203f65ce
conkobar <connorkleine@gmail.com>
2022-08-04 13:21:54 -0500

    still missing a check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!ht)
 		return (NULL);
 
-	ht->array = malloc(sizeof(hash_table_t) * size);
+	ht->array = malloc(sizeof(hash_table_t *) * size);
 
 	if (!ht->array)
 		return (NULL);
